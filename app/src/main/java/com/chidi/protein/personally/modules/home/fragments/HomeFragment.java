f2f3288aa0ef0adda3d5c7cab7d9ab8e48ec2b49
Chikrix <itschidi5s@gmail.com>
2017-12-20 22:25:27 +0100

    Fetch news items given query

--- app/src/main/java/com/chidi/protein/personally/modules/home/fragments/HomeFragment.java
+++ app/src/main/java/com/chidi/protein/personally/modules/home/fragments/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.chidi.protein.personally.modules.home.fragments;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,12 +10,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import com.chidi.protein.personally.R;
 import com.chidi.protein.personally.databinding.FragmentHomeBinding;
+import com.chidi.protein.personally.domain.models.NewsModel;
+import com.chidi.protein.personally.modules.home.viewmodel.HomeFragmentViewModel;
 
 public class HomeFragment extends Fragment {
   private static HomeFragment homeFragment;
   private FragmentHomeBinding fragmentHomeBinding;
+  private HomeFragmentViewModel homeFragmentViewModel;
 
   public HomeFragment() {
 
@@ -24,7 +30,29 @@ public class HomeFragment extends Fragment {
       @Nullable Bundle savedInstanceState) {
     fragmentHomeBinding =
         DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
+    homeFragmentViewModel = ViewModelProviders.of(this).get(HomeFragmentViewModel.class);
+    fragmentHomeBinding.setViewModel(homeFragmentViewModel);
+    observeViewModels();
     return fragmentHomeBinding.getRoot();
+  }
+
+  @Override public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    fetchNewsItems();
+  }
+
+  private void fetchNewsItems() {
+    homeFragmentViewModel.isShowingProgressBar.set(true);
+    homeFragmentViewModel.fetchNewsItems("technology");
+  }
+
+  private void observeViewModels() {
+    homeFragmentViewModel.newsItems.observe(this, new Observer<NewsModel>() {
+      @Override public void onChanged(@Nullable NewsModel newsModel) {
+        homeFragmentViewModel.isShowingProgressBar.set(false);
+        Toast.makeText(getContext(), "Messages found", Toast.LENGTH_SHORT).show();
+      }
+    });
   }
 
   public static HomeFragment newInstance() {
--- app/src/main/java/com/chidi/protein/personally/modules/home/viewmodel/HomeFragmentViewModel.java
+++ app/src/main/java/com/chidi/protein/personally/modules/home/viewmodel/HomeFragmentViewModel.java
@@ -1,8 +1,24 @@
 package com.chidi.protein.personally.modules.home.viewmodel;
 
-/**
- * Created by Chidi Justice on 20/12/2017.
- */
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.databinding.ObservableField;
+import com.chidi.protein.personally.domain.models.NewsModel;
+import com.chidi.protein.personally.domain.repositories.OnlineRepository;
 
-public class HomeFragmentViewModel {
+public class HomeFragmentViewModel extends ViewModel {
+
+  private OnlineRepository onlineRepository = OnlineRepository.getOnlineRepositoryInstance();
+  public ObservableField<Boolean> isShowingProgressBar = new ObservableField<>(true);
+
+  public MutableLiveData<NewsModel> newsItems = new MutableLiveData<>();
+
+  public void fetchNewsItems(String query) {
+    NewsModel newsArticles = onlineRepository.fetchNewsItems(query);
+    newsItems.setValue(newsArticles);
+  }
+
+  @Override protected void onCleared() {
+    onlineRepository = null;
+  }
 }
