3317e948c007ed2869059aa6044e0bc14bb5ec07
Chikrix <itschidi5s@gmail.com>
2017-12-26 19:28:42 +0100

    Update play services if not available or close app

--- app/src/main/java/com/chidi/protein/personally/modules/home/activities/HomeActivity.java
+++ app/src/main/java/com/chidi/protein/personally/modules/home/activities/HomeActivity.java
@@ -1,9 +1,11 @@
 package com.chidi.protein.personally.modules.home.activities;
 
+import android.app.Dialog;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 import com.chidi.protein.personally.R;
 import com.chidi.protein.personally.databinding.ActivityHomeBinding;
 import com.chidi.protein.personally.domain.models.Article;
@@ -11,6 +13,8 @@ import com.chidi.protein.personally.modules.home.fragments.HomeFragment;
 import com.chidi.protein.personally.modules.home.fragments.NewsDetailsFragment;
 import com.chidi.protein.personally.utils.Constants;
 import com.chidi.protein.personally.utils.SimpleFragmentManager;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class HomeActivity extends AppCompatActivity {
   private ActivityHomeBinding activityHomeBinding;
@@ -20,7 +24,17 @@ public class HomeActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     activityHomeBinding = DataBindingUtil.setContentView(this, R.layout.activity_home);
     simpleFragmentManager = new SimpleFragmentManager(getSupportFragmentManager());
-    simpleFragmentManager.replaceFragment(HomeFragment.newInstance(), R.id.homeActivityFlContainer, true);
+  }
+
+  @Override protected void onResume() {
+    super.onResume();
+    if (isGooglePlayServiceAvailable()) {
+      activityHomeBinding.homeActivityPbLoading.setVisibility(View.GONE);
+      simpleFragmentManager.replaceFragment(HomeFragment.newInstance(), R.id.homeActivityFlContainer,
+          true);
+    } else {
+      Toast.makeText(this, R.string.has_play_store, Toast.LENGTH_LONG).show();
+    }
   }
 
   @Override public void onBackPressed() {
@@ -34,4 +48,16 @@ public class HomeActivity extends AppCompatActivity {
         R.id.homeActivityFlContainer, true);
   }
 
+  private boolean isGooglePlayServiceAvailable() {
+    GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+    int status = apiAvailability.isGooglePlayServicesAvailable(this);
+    Dialog dialog = apiAvailability.getErrorDialog(this, status, 2404);
+    if (status != ConnectionResult.SUCCESS) {
+      if (apiAvailability.isUserResolvableError(status)) {
+        dialog.show();
+      }
+    }
+
+    return status == ConnectionResult.SUCCESS;
+  }
 }
